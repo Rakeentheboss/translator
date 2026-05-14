10ce350514641fe0a66e47123bbfbdfdf2cda0a6
RakeenTheBoss <islamrakeen8@gmail.com>
2026-05-15 02:30:32 +0600

    Updated translator app - fixed dotenv and API setup

--- translate.py
+++ translate.py
@@ -2,9 +2,9 @@ import streamlit as st
 from gtts import gTTS
 from openai import OpenAI
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+
+
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 st.set_page_config(
